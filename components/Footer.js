d367c25d30a5ee49e15c73e360b0d75de047a80a
mhanifmuhsin <m.hanif.muhsin@gmail.com>
2021-10-30 15:34:01 +0700

    :hammer:add footer content

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+export default function Footer(){
+    return (
+        <div className="max-w-screen-md bg-yellow-300 mx-auto font-lato my-7">
+            <ul className="flex justify-evenly list-none">
+                <li><a href = "mailto: m.hanif.muhsin@gmail.com">Send Email</a></li>
+                <li><a href = "https://github.com/mhanifmuhsin?tab=repositories">Github</a></li>
+                <li><a href = "https://www.linkedin.com/in/mhanifmuhsin">Linkedin</a></li>
+                <li><a href = "https://instagram.com/mhanifmuhsin">Instagram</a></li>
+            </ul>
+        </div>
+    )
+}
--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,11 @@
+import Footer from "./Footer"
 import Navbar from "./Navbar"
 export default function Layout({ children }) {
     return (
         <>
             <Navbar />
             <main className="max-w-screen-md mx-auto px-5">{children}</main>
+            <Footer />
         </>
     )
 }
